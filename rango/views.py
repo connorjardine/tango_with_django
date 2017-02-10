9bd654149b8308834e8df927d4f3bafb0980a513
Connor Jardine <2188232J@student.gla.ac.uk>
2017-02-10 10:36:34 +0000

    end of chapter 10

--- rango/views.py
+++ rango/views.py
@@ -10,8 +10,40 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+	
+# Updated the function definition
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+												'last_visit',
+												str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+									'%Y-%m-%d %H:%M:%S')
+
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		#update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		visits = 1
+		# set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+
+	# Update/set the visits cookie
+	request.session['visits'] = visits
+
 
 def index(request):
+	request.session.set_test_cookie()
 	# Query the database for a list of ALL categories currently stored.
 	# Order the categories by no. likes in descending order.
 	# Retrieve the top 5 only - or all if less than 5.
@@ -23,12 +55,23 @@ def index(request):
 	
 	context_dict = {'categories': category_list, 'pages': page_list}
 	
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+	
 	# Render the response and send it back!
 	response = render(request, 'rango/index.html',context = context_dict)
 	return response
 	
 def about(request):
-	return render(request, 'rango/about.html',)
+	request.session.set_test_cookie()
+	
+	context_dict = {}
+	
+	visitor_cookie_handler(request)
+	context_dict['visits'] =request.session['visits']
+	
+	response = render(request, 'rango/about.html',context = context_dict)
+	return response
 	
 def show_category(request, category_name_slug):
 	# Create a context dictionary which we can pass
